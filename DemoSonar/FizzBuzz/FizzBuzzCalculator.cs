72be45348a26a3dedf721efe47294f7e8ad3686f
Jonathan Aldave Mendoza <jgam777@hotmail.com>
2024-11-12 11:47:44 -0500

    fizzbuzz

---
+++ DemoSonar/FizzBuzz/FizzBuzzCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzz;
+public class FizzBuzzCalculator
+{
+    public string Calculate(int number)
+    {
+        if (number == 3)
+            return "Fizz";
+        return number.ToString();
+    }
+}
---
+++ DemoSonar/FizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FizzBuzz;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input:");
+
+        var inputString = Console.ReadLine();
+        var intputNumber = Convert.ToInt32(inputString);
+
+        var fizzBuzzCalulator = new FizzBuzzCalculator();
+
+        try
+        {
+            Console.WriteLine($"Output: {fizzBuzzCalulator.Calculate(intputNumber)}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+
+        Console.ReadKey();
+    }
+}
